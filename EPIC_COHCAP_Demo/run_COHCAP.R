e3d3ccffdd1b3439e4cdb57f42a541ed27227f1e
Charles Warden <cwarden45@gmail.com>
2017-02-08 12:08:11 -0800

    Update run_COHCAP.R

--- EPIC_COHCAP_Demo/run_COHCAP.R
+++ EPIC_COHCAP_Demo/run_COHCAP.R
@@ -1,30 +1,44 @@
-#setwd("C:\\Users\\Charles\\Documents\\COHCAP\\DNAmethylation_templates\\EPIC_COHCAP_Demo")
-library(COHCAP)
-
 beta.file = "minfi.txt"
 #above from minfi
 
 library(COHCAP)
 island.file = "additional_annotations/COHCAP_EPIC_UCSC_TSS1500_plus_1st_Exon.txt"
-sample.file <- "COHCAP_sample_description.txt"
-project.folder <- getwd()
-project.name <- "EPIC_Test"
+sample.file = "COHCAP_sample_description.txt"
+project.folder = getwd()
+project.name = "EPIC_Test"
 
-beta.table <- COHCAP.annotate(beta.file, project.name, project.folder, platform="custom", annotation.file = island.file)
+beta.table = COHCAP.annotate(beta.file, project.name, project.folder, platform="custom", annotation.file = island.file)
 COHCAP.qc(sample.file, beta.table, project.name, project.folder)
 
 #one-vs-one comparison
-sample.file <- "COHCAP_paired_description.txt"
-filtered.sites <- COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2")
+sample.file = "COHCAP_paired_description.txt"
 
-project.name <- "EPIC_Test_v2"
-filtered.sites <- COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1)
-filtered.islands <- COHCAP.avg.by.site(filtered.sites, project.name, project.folder)
+Island.Count = c()
+Site.Count = c()
 
-project.name <- "EPIC_Test_v3"
-filtered.sites <- COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1, methyl.cutoff=0.3)
-filtered.islands <- COHCAP.avg.by.site(filtered.sites, project.name, project.folder, methyl.cutoff=0.3)
+filtered.sites = COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2")
 
-project.name <- "EPIC_Test_v4"
-filtered.sites <- COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1, methyl.cutoff=0, unmethyl.cutoff=1, delta.beta = 0.1)
-filtered.islands <- COHCAP.avg.by.site(filtered.sites, project.name, project.folder, methyl.cutoff=0, unmethyl.cutoff=1, delta.beta = 0.1, fdr.cutoff=1, num.sites=2)
+project.name = "EPIC_Test_v2"
+filtered.sites = COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1)
+Site.Count[1] = nrow(filtered.sites)
+filtered.islands = COHCAP.avg.by.site(filtered.sites, project.name, project.folder)
+Island.Count[1] = nrow(filtered.islands)
+
+project.name = "EPIC_Test_v3"
+filtered.sites = COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1, methyl.cutoff=0.3)
+Site.Count[2] = nrow(filtered.sites)
+filtered.islands = COHCAP.avg.by.site(filtered.sites, project.name, project.folder, methyl.cutoff=0.3)
+Island.Count[2] = nrow(filtered.islands)
+
+project.name = "EPIC_Test_v4"
+filtered.sites = COHCAP.site(sample.file, beta.table, project.name, project.folder, ref="rep2", fdr.cutoff=1, pvalue.cutoff=1, methyl.cutoff=0, unmethyl.cutoff=1, delta.beta = 0.1)
+Site.Count[3] = nrow(filtered.sites)
+Site.Count[4] = nrow(filtered.sites)
+filtered.islands = COHCAP.avg.by.site(filtered.sites, project.name, project.folder, methyl.cutoff=0, unmethyl.cutoff=1, delta.beta = 0.1, fdr.cutoff=1)
+Island.Count[3] = nrow(filtered.islands)
+
+filtered.islands = COHCAP.avg.by.site(filtered.sites, project.name, project.folder, methyl.cutoff=0, unmethyl.cutoff=1, delta.beta = 0.1, fdr.cutoff=1, num.sites=2)
+Island.Count[4] = nrow(filtered.islands)
+
+comp.mat = data.frame(Island.Count, Site.Count)
+print(comp.mat)
